3eb02665e972c6a838dfa53d4cfbb5e59b0e3cbd
ryokcha <ryouka6922@icloud.com>
2025-11-22 01:30:42 +0900

    ヘッダーの要素

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 export function Navigation() {
   const links = [
-    { href: "/", label: "Home", id: "home" },
     { href: "/about", label: "About", id: "about" },
     { href: "/products", label: "Products", id: "products" },
     { href: "/contact", label: "Contact", id: "contact" },
@@ -12,7 +11,7 @@ export function Navigation() {
     <header className="rounded-b-3xl bg-[#567282]/80 backdrop-blur-xl dark:bg-[#567282]/80">
       <nav className="max-w-4xl mx-auto px-6 py-8 flex justify-between items-center">
         <Link href="/">
-          <h1 className="text-2xl font-bold text-[#6C8FA3] dark:text-white hover:text-[#D5848C] dark:hover:text-[#D5848C] transition-colors">
+          <h1 className="text-2xl font-bold text-[#c8d8db] dark:text-[#c8d8db] hover:text-[#D5848C] dark:hover:text-[#D5848C] transition-colors">
             n0ta
           </h1>
         </Link>
